c6828771a5117dc6a8aaea13dcaa038efd0b9958
Nathan Almeida <nathanalmeidadev@outlook.com>
2016-10-03 18:54:43 -0300

    Adicionado classe principal

---
+++ src/br/com/nathanalmeida/naskerlib/NaskerLib.java
@@ -0,0 +1,14 @@
+package br.com.nathanalmeida.naskerlib;
+
+/**
+ * Created by Neitan96 on 02/10/2016.
+ */
+public final class NaskerLib{
+
+    public static final String version = "1.0";
+    public static final int build = 1;
+
+    private NaskerLib(){
+    }
+
+}
